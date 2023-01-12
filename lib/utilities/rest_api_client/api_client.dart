f3736d28a452689edd407f7082fa1ab28de266c2
Tran Duy Thinh <thinhtran122000@gmail.com>
2023-01-12 22:36:24 +0700

    update code

--- lib/utilities/rest_api_client/api_client.dart
+++ lib/utilities/rest_api_client/api_client.dart
@@ -29,7 +29,44 @@ class RestAPIClient extends APIClient {
       );
       return APIResponse.fromJson(response.data);
     } on DioError catch (e) {
-      throw e.response?.data['message'] ?? '';
+      if (e.type == DioErrorType.connectTimeout) {
+        throw Exception('Connection Timeout Exception');
+      } else if (e.type == DioErrorType.receiveTimeout) {
+        throw Exception('Connection Receive Timeout Exception');
+      } else if (e.type == DioErrorType.cancel) {
+        throw Exception('Cancel Request Exception');
+      } else if (e.type == DioErrorType.other) {
+        throw Exception('Unknown Exception');
+      } else if (e.type == DioErrorType.sendTimeout) {
+        throw Exception('Send Timeout Exception');
+      } else if (e.type == DioErrorType.response) {
+        switch (e.response?.statusCode) {
+          case 400:
+            throw Exception('Bad Request');
+          case 401:
+            throw Exception('Authorization Exception');
+          case 403:
+            throw Exception('Forbidden');
+          case 404:
+            throw Exception('Not Found');
+          case 429:
+            throw Exception('Too Many Request');
+          case 500:
+            throw Exception('Internal Server Exception');
+          case 501:
+            throw Exception('Not Implemented');
+          case 502:
+            throw Exception('Bad Gateway');
+          case 503:
+            throw Exception('Service Unavailable');
+          case 504:
+            throw Exception('Gateway Timed Out');
+          default:
+            throw Exception('Invalid Exception');
+        }
+      } else {
+        throw e.response?.data['message'] ?? '';
+      }
     } catch (e) {
       rethrow;
     }
